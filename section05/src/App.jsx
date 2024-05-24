3cfd93b8a9ec0c1831e83bcaeb72f527951614cb
KOO612 <ksm99007@gmail.com>
2024-05-24 17:55:25 +0900

    state props 관리

---
+++ section05/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Bulb from '../components/Bulb';
+import Counter from '../components/Counter';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Bulb></Bulb>
+        <Counter></Counter>
+      </div>
+    </div>
+  );
+}
+
+export default App;
