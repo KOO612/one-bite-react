7ae525239d3b9341ad5503ccf5b9474847a94de2
KOO612 <ksm99007@gmail.com>
2024-05-30 17:41:33 +0900

    통합 이벤트 핸들러, 통합 state 상태관리

---
+++ section05/components/Register.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+const Register = () => {
+  const [input, setInput] = useState({
+    name: '',
+    birth: '',
+    country: '',
+    bio: '',
+  });
+
+  // 통합 이벤트 핸들러
+  const onChange = (e) => {
+    setInput({
+      ...input,
+      // name과 value 필수
+      // js 객체 관련 내용
+      // 객체 생성 시 키 위치에 [변수]를 사용하면 키로 설정됨
+      // 동적으로 결정하기 위해 [] 대괄호 사용
+      // name 속성의 값을 적용
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div>
+      <div>
+        <input name="name" type="text" value={input.name} onChange={onChange} placeholder={'이름'} />
+      </div>
+      <div>
+        <input name="birth" type="date" onChange={onChange} value={input.birth} />
+      </div>
+      <div>
+        <select name="country" value={input.country} onChange={onChange}>
+          <option value=""></option>
+          <option value="kr">한국</option>
+          <option value="us">미국</option>
+          <option value="uk">영국</option>
+        </select>
+      </div>
+      <div>
+        <textarea name="bio" value={input.bio} onChange={onChange} />
+      </div>
+    </div>
+  );
+};
+
+export default Register;
--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,14 +1,10 @@
+import Register from '../components/Register';
 import './App.css';
-import Bulb from '../components/Bulb';
-import Counter from '../components/Counter';
 
 function App() {
   return (
     <div>
-      <div>
-        <Bulb></Bulb>
-        <Counter></Counter>
-      </div>
+      <Register />
     </div>
   );
 }
