630cfce65406e9afc83e11736a1f81279a59ff0d
KOO612 <ksm99007@gmail.com>
2024-06-14 17:39:19 +0900

    이미지까지 완료

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -2,43 +2,25 @@ import './App.css';
 import Home from './pages/Home';
 import New from './pages/New';
 import Diary from './pages/Diary';
+import Edit from './pages/Edit';
 import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 import Notfound from './pages/Notfound';
+import Button from './components/Button';
+import Header from './components/Header';
 
-import emotion1 from './assets/emotion1.png';
-import emotion2 from './assets/emotion2.png';
-import emotion3 from './assets/emotion3.png';
-import emotion4 from './assets/emotion4.png';
-import emotion5 from './assets/emotion5.png';
+// import { getEmotionImage } from './util/get-emotion-image';
 
 // 1. "/" : 모든 일기를 조회하는 home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 
 function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav('/new');
-  };
   return (
     <>
-      <div>
-        <img src={emotion1} />
-        <img src={emotion2} />
-        <img src={emotion3} />
-        <img src={emotion4} />
-        <img src={emotion5} />
-      </div>
-      <div>
-        <Link to={'/'}>home</Link>
-        <Link to={'/new'}>New</Link>
-        <Link to={'/diary'}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>new 페이지 이동</button>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/new" element={<New />}></Route>
+        <Route path="/edit/:id" element={<Edit />}></Route>
         <Route path="/diary/:id" element={<Diary />}></Route>
         <Route path="*" element={<Notfound />}></Route>
       </Routes>
--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-const Diary = () => {
+const Edit = () => {
   const params = useParams();
-  return <div>{params.id}번 일기 입니다.</div>;
+  return <div>{params.id}번 일기</div>;
 };
 
-export default Diary;
+export default Edit;
